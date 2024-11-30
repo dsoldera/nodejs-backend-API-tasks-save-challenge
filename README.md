5ab3ef1bf84af662238b92428b6fe94ff885a9ba
dsoldera <daniellesoldera@gmail.com>
2024-11-30 16:16:09 -0300

    Update Challenge

--- README.md
+++ README.md
@@ -7,16 +7,17 @@ Nesse desafio você desenvolverá uma API para realizar o CRUD de suas *tasks* (
 
 A API deve conter as seguintes funcionalidades:
 
-- Criação de uma task
-- Listagem de todas as tasks
-- Atualização de uma task pelo `id`
-- Remover uma task pelo `id`
-- Marcar pelo `id` uma task como completa
-- E o verdadeiro desafio: Importação de tasks em massa por um arquivo CSV
+- [X] Criação de uma task
+- [X] Listagem de todas as tasks
+- [X] Listagem da task pelo `id`
+- [x] Atualização de uma task pelo `id`
+- [X] Remover uma task pelo `id`
+- [X] Marcar pelo `id` uma task como completa
+- [] E o verdadeiro desafio: Importação de tasks em massa por um arquivo CSV
 
 ### Rotas e regras de negócio
 
-Antes das rotas, vamos entender qual a estrutura (propriedades) que uma task deve ter:
+[x] Antes das rotas, vamos entender qual a estrutura (propriedades) que uma task deve ter:
 
 - `id` - Identificador único de cada task
 - `title` - Título da task
@@ -27,19 +28,19 @@ Antes das rotas, vamos entender qual a estrutura (propriedades) que uma task dev
 
 Rotas:
 
-- `POST - /tasks`
+- [x] `POST - /tasks`
     
     Deve ser possível criar uma task no banco de dados, enviando os campos `title` e `description` por meio do `body` da requisição.
     
     Ao criar uma task, os campos: `id`, `created_at`, `updated_at` e `completed_at` devem ser preenchidos automaticamente, conforme a orientação das propriedades acima.
     
-- `GET - /tasks`
+- [x] `GET - /tasks`
     
     Deve ser possível listar todas as tasks salvas no banco de dados.
     
     Também deve ser possível realizar uma busca, filtrando as tasks pelo `title` e `description`
     
-- `PUT - /tasks/:id`
+- [x] `PUT - /tasks/:id`
     
     Deve ser possível atualizar uma task pelo `id`.
     
@@ -49,18 +50,18 @@ Rotas:
     
     Antes de realizar a atualização, deve ser feito uma validação se o `id` pertence a uma task salva no banco de dados.
     
-- `DELETE - /tasks/:id`
+- [x] `DELETE - /tasks/:id`
     
     Deve ser possível remover uma task pelo `id`.
     
     Antes de realizar a remoção, deve ser feito uma validação se o `id` pertence a uma task salva no banco de dados.
     
-- `PATCH - /tasks/:id/complete`
+- [x] `PATCH - /tasks/:id/complete`
     
     Deve ser possível marcar a task como completa ou não. Isso significa que se a task estiver concluída, deve voltar ao seu estado “normal”.
     
     Antes da alteração, deve ser feito uma validação se o `id` pertence a uma task salva no banco de dados.
-    
+
 
 ### E a importação do CSV?
 
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,146 @@
-//import { randomUUID } from 'node:crypto'
+import { randomUUID } from 'node:crypto'
 import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
-export const routes = [{
-}]
+export const routes = [
+  {    
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { search } = req.query
+
+      const tasks = database.select('tasks', {
+        title: search,
+        description: search
+      })
+
+      return res.end(JSON.stringify(tasks))
+    }
+  },
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      const [task] = database.select('tasks', { id})
+
+      if (!task) {
+        return res.writeHead(404).end(
+          JSON.stringify({ message: 'Task nao localizada' })
+        )
+      }
+
+      return res.end(JSON.stringify(task))
+    }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { title, description } = req.body
+
+      if (!title) {
+        return res.writeHead(400).end(
+          JSON.stringify({ message: 'O campo title é obrigatorio' }),
+        )
+      }
+
+      if (!description) {
+        return res.writeHead(400).end(
+          JSON.stringify({message: 'O campo descrição é obrigatorio' })
+        )
+      }
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date(),
+      }
+
+      database.insert('tasks', task) // insert at the database
+
+      return res.writeHead(201).end(
+        JSON.stringify({ message: 'Task criada com sucesso!'})
+      )
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { title, description } = req.body
+
+      const [task] = database.select('tasks', { id })
+
+      if (!task) {
+        return res.writeHead(404).end(
+          JSON.stringify({ message: 'Task nao localizada' })
+        )
+      }
+
+      if (!title || !description) {
+        return res.writeHead(400).end(
+          JSON.stringify({ message: 'O campo title e a descrição são obrigatórios!' })
+        )
+      }
+
+      database.update('tasks', id, {
+        title: title ?? task.title,
+        description: description ?? task.description,
+        updated_at: new Date()
+      })
+
+      return res.writeHead(204).end(
+        JSON.stringify({ message: 'Task atualizada com sucesso!' })
+      )
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      const [task] = database.select('tasks', { id })
+
+      if (!task) {
+        return res.writeHead(404).end()
+      }
+
+      database.delete('tasks', id) // deleted the task
+
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'PATCH',
+    path: buildRoutePath('/tasks/:id/complete'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      const [task] = database.select('tasks', { id })
+
+      if (!task) {
+        return res.writeHead(404).end(
+          JSON.stringify({ message: 'Task nao localizada' })
+        )
+      }
+
+      const isTaskCompleted = !!task.completed_at
+      const completed_at = isTaskCompleted ? null : new Date()
+
+      database.update('tasks', id, { completed_at })
+
+      return res.writeHead(204).end(
+        JSON.stringify({ message: 'Task atualizada com sucesso!' })
+      )
+    }
+  }
+]
